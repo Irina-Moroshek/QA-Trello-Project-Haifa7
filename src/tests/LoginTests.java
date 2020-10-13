90c18ff40ec9214033d9011d9fffd6f8885a612c
Irina-Moroshek <irina_moroshek@mail.ru>
2020-10-13 10:51:34 +0300

    Sel-10, add all wait methods

--- src/tests/LoginTests.java
+++ src/tests/LoginTests.java
@@ -8,103 +8,134 @@ import org.testng.annotations.Test;
 public class LoginTests extends TestBase {
 
     @BeforeMethod
-    public void initTests() throws InterruptedException {
+    public void initTests() {
+
         //кликаем на кнопку войти
         WebElement loginIcon = driver.findElement(By.xpath("//*[@class='btn btn-sm btn-link text-white']"));
         loginIcon.click();
-        Thread.sleep(10000);
+        waitUntilElementIsClickable(By.xpath("//*[@class='btn btn-sm btn-link text-white']"),20);
     }
-    @Test
-    public void loginNegativeLoginEmpty() throws InterruptedException {
-        //находим поле password кликаем на него, очищаем, вводим пароль
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.click();
-        passwordField.clear();
-        passwordField.sendKeys(PASSWORD);
-        //находим кнопку ввод, кликаем на нее
-        WebElement loginButton = driver.findElement(By.id("login"));
-        loginButton.click();
-        //выводим сообщение об ошибке
-        System.out.println("Error: " + driver.findElement(By.id("error")).getText());
-    }
-    //Sel-04
-    @Test
-    public void loginNegativeNonexistentLoginAndPassword() throws InterruptedException {
-        // находим поле логин, кликаем, очищаем
-        WebElement loginField = driver.findElement(By.id("user"));
-        loginField.click();
-        loginField.clear();
-        //вводим в поле логин несуществующий логин
-        loginField.sendKeys("i_mor@mail.ru");
-        //находим поле password кликаем на него, очищаем
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.click();
-        passwordField.clear();
-        //вводим в поле логин несуществующий пароль
-        passwordField.sendKeys("1234567");
-        WebElement loginButton = driver.findElement(By.id("login"));
-        loginButton.click();
-        //выводим сообщение об ошибке
-        System.out.println("Error: " + driver.findElement(By.id("error")).getText());
-        Thread.sleep(2000);
 
-    }
-    //Sel-05
-    @Test
-    public void loginNegativeNonexistentPassword () throws InterruptedException {
-        // находим поле логин, кликаем, очищаем
-        WebElement loginField = driver.findElement(By.id("user"));
-        loginField.click();
-        loginField.clear();
-        //вводим в поле логин существующий логин
-        loginField.sendKeys(LOGIN);
-        Thread.sleep(2000);
-        //находим и нажимаем кнопку войти
-        WebElement loginButton = driver.findElement(By.id("login"));
-        loginButton.click();
-        //находим поле password кликаем на него, очищаем
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.click();
-        passwordField.clear();
-        //вводим в поле логин несуществующий пароль
-        passwordField.sendKeys("1234567");
-        //находим и нажимаем кнопку войти
-        WebElement login = driver.findElement(By.id("login-submit"));
-        login.click();
-        Thread.sleep(2000);
-        //выводим сообщение об ошибке
-        System.out.println("Error: "+ driver.findElement(By.cssSelector("#login-error")).getText());
-        Thread.sleep(2000);
+        @Test
+        public void loginNegativeLoginEmpty ()  {
+            //ждем пока элемент станет активным
+            waitUntilElementIsClickable(By.id("login"),10);
 
-    }
-    //Sel-06
-    @Test
-    public void loginPositiveTest () throws InterruptedException {
+        // находим поле password кликаем на него, очищаем, вводим пароль
+            WebElement passwordField = driver.findElement(By.id("password"));
+            passwordField.click();
+            passwordField.clear();
+            passwordField.sendKeys(PASSWORD);
+
+            //находим кнопку ввод, кликаем на нее
+            WebElement loginButton = driver.findElement(By.id("login"));
+            loginButton.click();
+
+            //ждем пока появится нужный элемент - сообщение об ошибке, чтобы элемент стал виден
+           // waitUntilElementIsPresent(By.id("error"), 30);
+            waitUntilElementIsVisible(By.id("error"), 30);
+            //выводим сообщение об ошибке
+            System.out.println("Error: " + driver.findElement(By.id("error")).getText());
+        }
+
+        //Sel-04
+        @Test
+        public void loginNegativeNonexistentLoginAndPassword ()  {
+            waitUntilElementIsClickable(By.id("login"),10);
+
+            // находим поле логин, кликаем, очищаем
+            WebElement loginField = driver.findElement(By.id("user"));
+            loginField.click();
+            loginField.clear();
+            //вводим в поле логин несуществующий логин
+            loginField.sendKeys("i_mor@mail.ru");
+
+            //находим поле password кликаем на него, очищаем
+            WebElement passwordField = driver.findElement(By.id("password"));
+            passwordField.click();
+            passwordField.clear();
+            //вводим в поле логин несуществующий пароль
+            passwordField.sendKeys("1234567");
+
+            WebElement loginButton = driver.findElement(By.id("login"));
+            loginButton.click();
+
+            //waitUntilElementIsClickable(By.id("error"),30);
+            waitUntilElementIsVisible(By.id("error"), 30);
+            //выводим сообщение об ошибке
+            System.out.println("Error: " + driver.findElement(By.id("error")).getText());
+
+        }
+
+        //Sel-05
+        @Test
+        public void loginNegativeNonexistentPassword () {
+            waitUntilElementIsClickable(By.id("login"),10);
+           // находим поле логин, кликаем, очищаем
+            WebElement loginField = driver.findElement(By.id("user"));
+            loginField.click();
+            loginField.clear();
+            //вводим в поле логин существующий логин
+            loginField.sendKeys(LOGIN);
+
+            //находим и нажимаем кнопку войти
+            WebElement loginButton = driver.findElement(By.id("login"));
+            loginButton.click();
+
+            waitUntilElementIsClickable(By.id("login-submit"), 30);
+           //находим поле password кликаем на него, очищаем
+            WebElement passwordField = driver.findElement(By.id("password"));
+            passwordField.click();
+            passwordField.clear();
+            //вводим в поле логин несуществующий пароль
+            passwordField.sendKeys("1234567");
+
+            //находим и нажимаем кнопку войти
+            WebElement login = driver.findElement(By.id("login-submit"));
+            login.click();
+
+            //waitUntilElementIsClickable(By.id("error"),30);
+            waitUntilElementIsVisible(By.cssSelector("#login-error"), 30);
+            //выводим сообщение об ошибке
+            System.out.println("Error: " + driver.findElement(By.cssSelector("#login-error")).getText());
+
+
+        }
+
+        //Sel-06
+        @Test
+        public void loginPositiveTest ()  {
+            waitUntilElementIsClickable(By.id("login"),10);
+
         // находим поле логин, кликаем, очищаем
-        WebElement loginField = driver.findElement(By.id("user"));
-        loginField.click();
-        loginField.clear();
-        //вводим в поле логин существующий логин
-        loginField.sendKeys(LOGIN);
-        Thread.sleep(5000);
-        //находим и нажимаем кнопку войти
-        WebElement loginButton = driver.findElement(By.id("login"));
-        loginButton.click();
-        Thread.sleep(5000);
-        //находим поле password кликаем на него, очищаем
-        WebElement passwordField = driver.findElement(By.id("password"));
-        passwordField.click();
-        passwordField.clear();
-        //вводим в поле логин существующий пароль
-        passwordField.sendKeys(PASSWORD);
-        Thread.sleep(5000);
-        //находим и нажимаем кнопку войти
-        WebElement login = driver.findElement(By.id("login-submit"));
-        login.click();
-        Thread.sleep(10000);
-        System.out.println("Boards button text: " + driver.findElement(By.xpath("//button[@data-test-id='header-boards-menu-button']")).getText());
-        Thread.sleep(2000);
+            WebElement loginField = driver.findElement(By.id("user"));
+            loginField.click();
+            loginField.clear();
+            //вводим в поле логин существующий логин
+            loginField.sendKeys(LOGIN);
+
+            waitUntilElementIsClickable(By.id("login"),10);
+            //находим и нажимаем кнопку войти
+            WebElement loginButton = driver.findElement(By.id("login"));
+            loginButton.click();
+
+            waitUntilElementIsClickable(By.id("login-submit"), 10);
+            //находим поле password кликаем на него, очищаем
+            WebElement passwordField = driver.findElement(By.id("password"));
+            passwordField.click();
+            passwordField.clear();
+            //вводим в поле логин существующий пароль
+            passwordField.sendKeys(PASSWORD);
+
+            //находим и нажимаем кнопку войти
+            WebElement login = driver.findElement(By.id("login-submit"));
+            login.click();
+
+            waitUntilElementIsClickable(By.xpath("//button[@data-test-id ='header-boards-menu-button']"),20);
+            //waitUntilElementIsVisible(By.xpath("//button[@data-test-id='header-boards-menu-button']"), 30);
+            System.out.println("Boards button text: " + driver.findElement(By.xpath("//button[@data-test-id='header-boards-menu-button']")).getText());
+
+        }
     }
-}
 
 
--- src/tests/TestBase.java
+++ src/tests/TestBase.java
@@ -1,7 +1,10 @@
 package tests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -9,23 +12,81 @@ public class TestBase {
     public static final String LOGIN = "irina_moroshek@mail.ru";
     public static final String PASSWORD = "iae062322";
 
-   //вынесли определние driver сюда, чтоб в других классах можно было инициализировать
+    //вынесли определние driver сюда, чтоб в других классах можно было инициализировать
     WebDriver driver;
+
 
     @BeforeMethod
     //метод, который открывает сайт https://trello.com/ общий для всех классов
     public void OpenAppl() throws InterruptedException {
         driver = new ChromeDriver();
         driver.get("https://trello.com/");
-        Thread.sleep(10000);
+        waitUntilElementIsWebsite("https://trello.com/", 10);
+        // Thread.sleep(10000);
     }
 
-    @AfterMethod
+     @AfterMethod
     //метод, котрый прекращает работу, общий для всех классов
-    public void tearDown(){
+    public void tearDown() {
 
         driver.quit();
     }
 
+    //проверьте этот код,я придумала, для задержки веб сайта,можно так написать код?
+    //ждем, пока элемент загрузится вебсайт
+    public void waitUntilElementIsWebsite(String url, int time) {
+        try {
+            new WebDriverWait(driver, time).until(ExpectedConditions.urlToBe(url));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //ждем, пока элемент не станет активным
+    public void waitUntilElementIsClickable(By locator, int time) {
+        try {
+            new WebDriverWait(driver, time).until(ExpectedConditions.elementToBeClickable(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //ждем, пока не появится элемент
+    public void waitUntilElementIsPresent(By locator, int time) {
+        try {
+            new WebDriverWait(driver, time).until(ExpectedConditions.presenceOfElementLocated(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //ждем, пока элемент не станет невидимым
+    public void waitUntilElementIsInvisible(By locator, int time) {
+        try {
+            new WebDriverWait(driver,time).until(ExpectedConditions.invisibilityOfElementLocated(locator));
+       } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //ждем, пока элемент не станет видимым
+    public void waitUntilElementIsVisible(By locator, int time) {
+        try {
+            new WebDriverWait(driver,time).until(ExpectedConditions.visibilityOfElementLocated(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    //ждем, пока элементы не станут видны
+    public void waitUntilElementsAreVisible (By locator, int time) {
+        try {
+            new WebDriverWait(driver,time).until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
 
